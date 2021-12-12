f7802379171ffba76870de87ad030eaa518aa423
Kalenghil <Kalenghil@users.noreply.github.com>
2021-12-12 22:42:30 +0300

    Made things easier

--- Классная работа/Декабрь/08.12.2021/26/25.py
+++ Классная работа/Декабрь/08.12.2021/26/25.py
@@ -1,9 +1,9 @@
-file_name = r'D:\WORK\11 класс\Макиевский Кирилл\Материалы\26data\26-k5.txt'
+file_name = r'C:\Users\Bosnian\Desktop\School_Work\Материалы\26data\26-k5.txt'
 with open(file_name, 'r') as f:
 	n, k, m = list(map(int, f.readline().split()))
 	nums = list(map(int, f.readlines()))
 
-nums.sort(reverse=True)
-cheapest_phones = list(reversed(nums))[:k]
-premium_phones = nums[:m]
+nums.sort()
+cheapest_phones = nums[:k]
+premium_phones = nums[-m:]
 print(min(premium_phones), sum(cheapest_phones) // len(cheapest_phones))
