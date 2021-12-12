a4f1acf2973db3fce7332a1164d8d7d537518dae
Kalenghil <Kalenghil@users.noreply.github.com>
2021-12-12 22:44:30 +0300

    Changed wrong file destination

--- Классная работа/Декабрь/08.12.2021/26/25.py
+++ Классная работа/Декабрь/08.12.2021/26/25.py
@@ -1,4 +1,4 @@
-file_name = r'C:\Users\Bosnian\Desktop\School_Work\Материалы\26data\26-k5.txt'
+file_name = r'D:\WORK\11 класс\Макиевский Кирилл\Материалы\26data\26-k5.txt'
 with open(file_name, 'r') as f:
 	n, k, m = list(map(int, f.readline().split()))
 	nums = list(map(int, f.readlines()))
